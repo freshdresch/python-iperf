aac11cd7c217254db03b2d5ae93f7f1cdd7b303d
Adam Drescher <adam.r.drescher@gmail.com>
2014-11-19 02:31:10 -0600

    Working on parser

---
+++ Parser.py
@@ -0,0 +1,34 @@
+# Assumes the output file is already in the folder
+# For each file, we want the total bits per second, MSS, interval 1-5 bits per second, host cpu utilization, and remote cpu utilization
+
+import json
+import sys
+from math import floor
+from pprint import pprint
+
+class Parser:
+    """ A class to extract any useful information from the iperf3
+    json output. Then users can access whatever fields they
+    are interested in. """
+
+    def __init__(self):
+        self.bps = 0
+        self.bytes = 0
+        self.runLength = 0
+        self.intervalBPS = []
+        self.intervalLength = 0
+        self.hostUtilization = 0
+        self.remoteUtilization = 0
+
+    def extract(self, file):
+        with open(file) as dataFile:
+            data = json.load(dataFile)
+
+        self.bps = data['end']['sum_received']['bits_per_second']
+        self.bytes = data['end']['sum_received']['bytes']
+        self.runLength = data['end']['sum_received']['seconds']
+        self.hostUtilization = data['end']['cpu_utilization_percent']['host_total'] / 100.0
+        self.remoteUtilization = data['end']['cpu_utilization_percent']['remote_total'] / 100.0
+        self.intervalLength = floor(data['intervals'][0]['sum']['seconds'])
+        for i in xrange(len(data['intervals'])):
+            self.intervalBPS.append(data['intervals'][i]['sum']['bits_per_second'])
