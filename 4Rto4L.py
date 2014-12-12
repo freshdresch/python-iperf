c54f3db469019c199ebe5e056752e4e521484f70
Adam Drescher <adam.r.drescher@gmail.com>
2014-12-12 14:34:16 -0600

    switched ips

--- 4Rto4L.py
+++ 4Rto4L.py
@@ -12,38 +12,38 @@ if len(sys.argv) != 2:
 password = sys.argv[1]
 
 # Server setup
-srv1 = Server("vm12c10v01", "192.168.1.2", password)
+srv1 = Server("vm12c10v01", "192.168.2.5", password)
 srv1.setType("tcp")
 srv1.listen("5201")
 srv1.start("5")
 
-srv2 = Server("vm12c10v02", "192.168.1.1", password)
+srv2 = Server("vm12c10v02", "192.168.2.6", password)
 srv2.setType("tcp")
 srv2.listen("5201")
 srv2.start("5")
 
-srv3 = Server("vm12c10v03", "192.168.1.4", password)
+srv3 = Server("vm12c10v03", "192.168.2.8", password)
 srv3.setType("tcp")
 srv3.listen("5201")
 srv3.start("5")
 
-srv4 = Server("vm12c10v04", "192.168.1.3", password)
+srv4 = Server("vm12c10v04", "192.168.2.7", password)
 srv4.setType("tcp")
 srv4.listen("5201")
 srv4.start("5")
 
 # Client setup
-cli1 = Client("vm12c11v01", "192.168.1.5", password)
-cli1.setTarget("192.168.1.1", "5201")
+cli1 = Client("vm12c11v01", "192.168.2.1", password)
+cli1.setTarget("192.168.2.5", "5201")
 
-cli2 = Client("vm12c12v01", "192.168.1.6", password)
-cli2.setTarget("192.168.1.2", "5201")
+cli2 = Client("vm12c12v01", "192.168.2.2", password)
+cli2.setTarget("192.168.2.6", "5201")
 
-cli3 = Client("vm12c13v01", "192.168.1.7", password)
-cli3.setTarget("192.168.1.3", "5201")
+cli3 = Client("vm12c13v01", "192.168.2.3", password)
+cli3.setTarget("192.168.2.8", "5201")
 
-cli4 = Client("vm12c14v01", "192.168.1.8", password)
-cli1.setTarget("192.168.1.4", "5201")
+cli4 = Client("vm12c14v01", "192.168.2.4", password)
+cli1.setTarget("192.168.2.7", "5201")
 
 thread1 = threading.Thread(target=partial(cli1.run, "5", "30"))
 thread2 = threading.Thread(target=partial(cli2.run, "5", "30"))
